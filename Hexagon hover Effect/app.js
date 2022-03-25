9c89832fcd38816c020ed6a0f52cb1c2a5e84550
Medo <mohammed.yuossry@gmail.com>
2022-03-26 01:08:55 +0200

    move to vanilla js

--- Hexagon hover Effect/app.js
+++
@@ -1,9 +0,0 @@
-const curser = document.querySelector(".curser");
-
-document.addEventListener("mousemove", (e) => {
-  let x = e.clientX,
-    y = e.clientY;
-
-  curser.style.left = `${x}px`;
-  curser.style.top = `${y}px`;
-});
